c64770e84046113246d2fd7fb7cb5d05ee435545
Edward Tanko <eddytnk@gmail.com>
2018-11-23 13:44:29 -0600

    update pipeline with bash script

--- ci/test-and-build.sh
+++ ci/test-and-build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 set -e -u -
- cd source-code-from-github/
-./mvnw clean package
-cp source-code-from-github/target/demo-1.0.jar build/demo.jar
+cd source-code-from-github/
+mvn clean package
+cp source-code-from-github/target/demo-1.0.jar  build-jar-file/demo.jar
